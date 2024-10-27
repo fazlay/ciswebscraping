daacc0a275b3dcdb560b13e76d5d45e6d09548f1
fazlay <rfazlay21@gmail.com>
2024-10-27 14:34:43 +0600

    update the app

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def start_scraping():
     try:
-        scraper()  # calls the original scraper function
-        return 'Scraping completed! Check the CSV files for results.'
+        scraper_data= scraper()  # calls the original scraper function
+        return scraper_data
     except Exception as e:
         return f'Error occurred: {str(e)}', 500
 
